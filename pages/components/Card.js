5156807d8a74c3641e9942c4ae1e6a3ac837fde2
lnoueryo <jounetsism@gmail.com>
2022-09-09 12:49:25 +0900

    カードにタイトル累加

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -1,16 +1,19 @@
 import styles from '../../styles/Home.module.css'
 import { useDispatch } from 'react-redux'
-import { changeAudioSrc } from "../../store/modules/audio";
-export default function Card({id, title}) {
+import { selectMusic } from "../../store/modules/audio";
+export default function Card(props) {
   const dispatch = useDispatch()
-  const onSelectMusic = (title) => {
-    const action = changeAudioSrc(title)
+  const onSelectMusic = (music) => {
+    const action = selectMusic(music)
     dispatch(action)
+  }
+  const changeTitle = (title) => {
+    return title.replace('/-/g', ' ');
   }
   return (
     <div className={styles.grid}>
-      <div className={styles.card} onClick={() => onSelectMusic(title)}>
-        <h2>{title}</h2>
+      <div className={styles.card} onClick={() => onSelectMusic(props)}>
+        <h2>{props.title && changeTitle(props.title)}</h2>
         <p>Find in-depth information about Next.js features and API.</p>
       </div>
     </div>
