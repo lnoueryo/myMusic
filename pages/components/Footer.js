0eb206deec3aa1483f56086a322b69158fc30f9d
lnoueryo <jounetsism@gmail.com>
2022-09-09 12:50:03 +0900

    コントローラー操作追加

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -2,87 +2,206 @@ import { useEffect, useRef, useState } from "react";
 import styles from '../../styles/Home.module.css'
 import store from '../../store'
 import Icon from '@mdi/react'
-import { mdiPlay, mdiFastForward, mdiRewind, mdiPause, mdiSkipNext, mdiSkipPrevious  } from '@mdi/js';
+import { mdiPlay, mdiFastForward, mdiRewind, mdiPause, mdiSkipNext, mdiSkipPrevious, mdiRepeatOnce   } from '@mdi/js';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux'
-import { changeDuration } from "../../store/modules/audio";
+import { changeDuration, changeCurrentTime, selectMusic, changeRepeatToggle } from "../../store/modules/audio";
 export default function Footer() {
-  const dispatch = useDispatch()
-  const selectedMusic = useSelector(state => state.audio.audioSrc);
-  // const duration = useSelector(state => state.audio.duration);
-  let duration
-  const MIN_RANGE = 0
-  const MAX_RANGE = 1000
-  const [playToggle, setPlayToggle] = useState(true)
+  const MINIMUM_MUSIC_ID = 1;
+  const dispatch = useDispatch();
+  const selectedMusic = useSelector(state => state.audio.selectedMusic);
+  const music = useSelector(state => state.audio.music);
+  const currentTime = useSelector(state => state.audio.currentTime);
+  const [playToggle, setPlayToggle] = useState(true);
+  const [isMouse, setIsMouse] = useState(false);
   const audio = useRef();
-  const range = useRef();
-  const onPlayOrPause = () => {
-    playToggle ? audio.current.play() : audio.current.pause();
-    setPlayToggle(prev => !prev);
-  };
-  const onChangeTime = (e) => {
-    // range.current.value = e.target.value;
-    audio.current.currentTime = e.target.value / MAX_RANGE * duration;
-  }
+  const rangeBarFrame = useRef();
+  const rangeBar = useRef();
   useEffect(() => {
-    range.current.value = 0;
     if(audio) {
-      duration = audio.current.duration;
       audio.current.addEventListener('timeupdate', (e) => {
-        range.current.value = MAX_RANGE / duration * e.target.currentTime;
+        const action = changeCurrentTime(e.target.currentTime)
+        dispatch(action)
+        rangeBar.current.style.width = (Math.round((100 / audio.current.duration * e.target.currentTime) * 5) / 5) + '%'
       });
       audio.current.addEventListener('durationchange', () => {
-        console.log('Hello')
-        duration = audio.current.duration;
-        // range.current.value = 0;
+        play(audio.current)
+      });
+      audio.current.addEventListener('ended', () => {
+        if(store.getState().audio.repeatToggle) return play(audio.current);
+        skipMusic(1)
       });
     }
   }, [audio]);
+  const play = (audio) => {
+    rangeBar.current.style.width = '0%'
+    const action = changeDuration(audio.duration);
+    dispatch(action)
+    audio.play();
+    setPlayToggle(false);
+  }
+  const skipMusic = (nextNum) => {
+    const selectedMusic = store.getState().audio.selectedMusic;
+    let nextId = selectedMusic.id + nextNum;
+    if(nextId == 0) nextId = music.length;
+    else if(music.length < nextId) nextId = MINIMUM_MUSIC_ID;
+    const nextMusic = music.find(v => v.id == nextId);
+    const action = selectMusic(nextMusic);
+    dispatch(action);
+  }
+  const forwardOrRewind = (nextNum) => {
+    rangeBar.current.style.transition = 'initial';
+    audio.current.currentTime += nextNum;
+    const action = changeCurrentTime(audio.current.currentTime)
+    dispatch(action)
+    rangeBar.current.style.transition = 'all 1s';
+  }
+  const emptyObj = (obj) => Object.keys(obj).length == 0;
+  const onPlayOrPause = () => {
+    if(emptyObj(selectedMusic)) return;
+    playToggle ? audio.current.play() : audio.current.pause();
+    setPlayToggle(prev => !prev);
+  };
+  const onPreventDrag = (e) => {
+    e.preventDefault();
+  }
+  const onDragStart = (e) => {
+    if(emptyObj(selectedMusic)) return;
+    rangeBar.current.style.transition = 'initial';
+    const ratio = e.clientX / rangeBarFrame.current.getBoundingClientRect().width;
+    rangeBar.current.style.width = (Math.round((ratio * 100) * 5) / 5) + '%';
+    audio.current.currentTime = audio.current.duration * ratio;
+    const action = changeCurrentTime(audio.current.currentTime)
+    dispatch(action)
+    setIsMouse(!isMouse)
+  }
+  const onDrag = (e) => {
+    if(isMouse) {
+      const ratio = e.clientX / rangeBarFrame.current.getBoundingClientRect().width;
+      rangeBar.current.style.width = (Math.round((ratio * 100) * 5) / 5) + '%';
+      audio.current.currentTime = audio.current.duration * ratio;
+      const action = changeCurrentTime(audio.current.currentTime)
+      dispatch(action)
+    }
+  }
+  const onDragEnd = () => {
+    if(!isMouse) return;
+    rangeBar.current.style.transition = 'all 1s';
+    setIsMouse(!isMouse)
+  }
+  const changeTimeFormat = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time - minutes * 60);
+    return `${minutes}:${seconds >= 10 ? seconds : '0' + seconds}`;
+  }
+  const changeTitle = (title) => {
+    return title.replace('/-/g', ' ');
+  }
   return (
-    <footer className={styles.footer}>
-      {
-        audio &&
-      <audio ref={audio} src={'/' + (selectedMusic || 'sakura') + '.mp3'}>
-        あなたのブラウザーは <code>audio</code>要素をサポートしていません。
-      </audio>
-      }
-      <div className={styles.range}>
-        <input type="range" min={MIN_RANGE} max={MAX_RANGE} ref={range} step="1" onChange={onChangeTime} />
-      </div>
-      <div>
-        <div className={styles['song-name']}>
-          {selectedMusic}
+    <>
+      {isMouse && <div onMouseOut={onDragEnd} onMouseUp={onDragEnd} onMouseMove={onDrag} style={{position: 'fixed', top: 0, bottom: 0, right: 0, left: 0, zIndex: 1000, cursor:'pointer'}}></div>}
+      <footer className={styles.footer}>
+        {
+          audio &&
+        <audio ref={audio} src={'/' + (selectedMusic?.title) + '.mp3'}>
+          あなたのブラウザーは <code>audio</code>要素をサポートしていません。
+        </audio>
+        }
+        <div className={styles.range}>
+          <div ref={rangeBarFrame} onMouseDown={onDragStart} onMouseUp={onDragEnd} onDragStart={onPreventDrag} style={{width: '100%', height: '10px', cursor:'pointer'}}>
+            <div ref={rangeBar} style={{width: '0%', height: '5px', cursor:'pointer', backgroundColor: 'red', transition: 'all 1s'}}></div>
+          </div>
         </div>
         <div>
-          <Icon path={mdiRewind}
-            className={styles['controller-icon']}
-            size={2}
-            color="red"
-          />
-          {
-            playToggle ?
-            <Icon path={mdiPlay}
+          <div className={styles['song-name']}>
+            {emptyObj(selectedMusic) || changeTitle(selectedMusic.title)}
+          </div>
+          <div style={{textAlign: 'center'}}>
+            {changeTimeFormat(currentTime)} / {changeTimeFormat(store.getState().audio.duration)}
+          </div>
+          <div>
+            <Icon path={mdiSkipPrevious}
               className={styles['controller-icon']}
               size={2}
               color="red"
-              onClick={onPlayOrPause}
+              onClick={() => skipMusic(-1)}
             />
-            :
-            <Icon path={mdiPause}
+            <Icon path={mdiRewind}
               className={styles['controller-icon']}
               size={2}
               color="red"
-              onClick={onPlayOrPause}
+              onClick={() => {forwardOrRewind(-5)}}
             />
+            {
+              playToggle ?
+              <Icon path={mdiPlay}
+                className={styles['controller-icon']}
+                size={2}
+                color="red"
+                onClick={onPlayOrPause}
+              />
+              :
+              <Icon path={mdiPause}
+                className={styles['controller-icon']}
+                size={2}
+                color="red"
+                onClick={onPlayOrPause}
+              />
 
-          }
-          <Icon path={mdiFastForward}
-            className={styles['controller-icon']}
-            size={2}
-            color="red"
-          />
+            }
+            <Icon path={mdiFastForward}
+              className={styles['controller-icon']}
+              size={2}
+              color="red"
+              onClick={() => forwardOrRewind(5)}
+            />
+            <Icon path={mdiSkipNext}
+              className={styles['controller-icon']}
+              size={2}
+              color="red"
+              onClick={() => skipMusic(1)}
+            />
+          </div>
+          <div>
+            <Icon path={mdiRepeatOnce}
+              className={styles['controller-icon']}
+              size={2}
+              color={store.getState().audio.repeatToggle ? 'red' : 'gray'}
+              onClick={() => {
+                if(emptyObj(selectedMusic)) return;
+                dispatch(changeRepeatToggle(!store.getState().audio.repeatToggle))
+              }}
+            />
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   )
 }
+// {/* <div>
+//   <span class="star5_rating ng-scope" data-rate="1" ng-if="$ctrl.isField('評価')"></span>
+//   <span class="ng-binding">1 年前</span>
+// </div> */}
+// .star5_rating {
+//   position: relative;
+//   z-index: 0;
+//   display: inline-block;
+//   white-space: nowrap;
+//   margin-right: 10px;
+//   color: #CCCCCC;
+// }
+// .star5_rating:before, .star5_rating:after {
+//   content: '★★★★★';
+// }
+// .star5_rating:after {
+//   position: absolute;
+//   z-index: 1;
+//   top: 0;
+//   left: 0;
+//   overflow: hidden;
+//   white-space: nowrap;
+//   color: #ffcf32;
+// }
+// .star5_rating[data-rate="1"]:after {
+//   width: 20%;
+// }
--- store/modules/audio.js
+++ store/modules/audio.js
@@ -3,27 +3,47 @@ import { createSlice } from '@reduxjs/toolkit';
 const audio = createSlice({
   name: 'audio',
   initialState: {
-    audioSrc: '',
+    selectedMusic: {},
     duration: 0,
-    songs: [
-      {id: 1, title: 'sakura', description: 'Have you felt something??'},
-      {id: 2, title: 'agape', description: 'Have you felt something??'},
+    currentTime: 0,
+    repeatToggle: false,
+    music: [
+      {id: 1, title: 'Sakura', description: 'Have you felt something??'},
+      {id: 2, title: 'Agape', description: 'Have you felt something??'},
+      {id: 3, title: 'Drive', description: 'Have you felt something??'},
+      {id: 4, title: 'Letter', description: 'Have you felt something??'},
+      {id: 5, title: 'Dimension', description: 'Have you felt something??'},
+      {id: 6, title: 'With', description: 'Have you felt something??'},
+      {id: 7, title: 'Brain-parasite', description: 'Have you felt something??'},
+      {id: 8, title: 'TDM', description: 'Have you felt something??'},
+      {id: 9, title: 'TDMdisco', description: 'Have you felt something??'},
+      {id: 10, title: 'Intro', description: 'Have you felt something??'},
+      {id: 11, title: 'New-world', description: 'Have you felt something??'},
+      {id: 12, title: 'Look-at-the-red-sky', description: 'Have you felt something??'},
+      {id: 13, title: 'Move-forward', description: 'Have you felt something??'},
+      {id: 14, title: 'Tell-me-anything', description: 'Have you felt something??'},
     ]
   },
   reducers: {
-    changeAudioSrc(state, { type, payload }) {
-      console.log(type, payload)
-      state.audioSrc = payload
+    selectMusic(state, { type, payload }) {
+      state.selectedMusic = payload;
       return state;
     },
     changeDuration(state, { type, payload }) {
-      console.log(type, payload)
-      state.duration = payload
+      state.duration = payload;
+      return state;
+    },
+    changeCurrentTime(state, { type, payload }) {
+      state.currentTime = payload;
+      return state;
+    },
+    changeRepeatToggle(state, { type, payload }) {
+      state.repeatToggle = payload;
       return state;
     },
   }
 })
 
-const { changeAudioSrc, changeDuration } = audio.actions;
-export { changeAudioSrc, changeDuration }
+const { selectMusic, changeDuration, changeCurrentTime, changeRepeatToggle } = audio.actions;
+export { selectMusic, changeDuration, changeCurrentTime, changeRepeatToggle }
 export default audio.reducer
