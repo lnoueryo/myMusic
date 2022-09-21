f6e0ac9adb92474a801a31d7da5d315e6087602e
lnoueryo <jounetsism@gmail.com>
2022-09-22 03:50:34 +0900

    チップ作成

--- pages/components/ArticleCards.js
+++ pages/components/ArticleCards.js
@@ -1,7 +1,9 @@
-import { Image, Icon, mdiCloseCircleOutline } from '@chakra-ui/react'
-import { useRouter } from 'next/router';
+/** @jsxImportSource @emotion/react */
 
-export default function ArticleCards({articles, tag}) {
+import { useRouter } from 'next/router';
+import { css } from '@emotion/react';
+import Chip from './Chip'
+export default function ArticleCards({tag}) {
   const articleURL = '/articles/'
   const router = useRouter();
   const movePage = (article) => router.push(articleURL + article.id)
@@ -10,39 +12,51 @@ export default function ArticleCards({articles, tag}) {
     router.push('/' + tag.category.name.toLowerCase() + '/' + tag.id)
   }
   return (
-    <div style={{fontFamily: '"游ゴシック体", "Yu Gothic", YuGothic, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif'}}>
-      <h3 style={{fontWeight: 'bold',fontSize: '24px'}}>{tag?.title}</h3>
-      <div style={{width: '100%', maxWidth: '1000px', margin: 'auto', position: 'relative', display: 'flex'}}>
-        <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap', marginRight: '300px', width: '100%', padding: '30px', borderRadius: '3px', filter: 'drop-shadow(0px 0px 3px rgba(0,0,0,.1))'}}>
-          {
-            articles && articles.map(article => {
-              return (
-                <div key={article.id} onClick={() => movePage(article)} style={{width: '100%', height: '150px', backgroundColor: 'white', display: 'flex', alignItems: 'center', margin: '15px', position: 'relative'}}>
-                  <Image style={{width: '150px', padding: '0 5px'}} src={`https://storage.googleapis.com/tech-blog-static/${tag.category.name.toLowerCase()}/${tag.src}`} />
-                  <div style={{width: '100%', height: '100%', padding: '5px 10px', display: 'flex', flexDirection: 'column'}}>
-                    <h3 style={{fontWeight: 'bold', fontSize: '14px', marginBottom: '10px'}}>{article?.title}</h3>
-                    <p style={{color: '#99a9b0', fontSize: '10px'}}>{article.description}</p>
-                    <div style={{marginTop: 'auto', justifyContent: 'end', display: 'flex', flexWrap: 'wrap-reverse'}}>
-                    {
-                    article.tags.length == 0 ? <div style={{marginRight: '10px'}}>なし</div> : article.tags.map(tag => {
-                      return (
-                        <div key={tag.id} onClick={(e) => {moveTagPage(e, tag)}} style={{margin: '3px', borderRadius: '20px', minWidth: '80px', backgroundColor: '#f4f4f7', display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '5px 15px'}}>
-                          <div style={{display: 'flex', alignItems: 'center'}}>
-                            <Image style={{width: '12px'}} src={`https://storage.googleapis.com/tech-blog-static/${tag.category.name.toLowerCase()}/${tag.src}`} />
-                            <div style={{padding: '0 7px', fontSize: '8px', fontWeight: 'bold'}}>{tag.name}</div>
-                          </div>
-                        </div>
-                      )
+    <>
+      {
+        tag.blogs && tag.blogs.map(blog => {
+          return (
+            <div className="w100 flex-align-center rel" css={cardFrame} key={blog.id} onClick={() => movePage(blog)}>
+              <img css={leftImage}  src={process.env.CATEGORY_URL + tag.category.name.toLowerCase() + '/' + tag.src} />
+              <div className="w100 h100 box" css={box}>
+                <h3 className="bold font14 mb10">{blog?.title}</h3>
+                <p className="font10" css={description}>{blog.description}</p>
+                <div className="box-bottom flex" css={tagContainer}>
+                  {
+                    blog.tags.length == 0 ? <div className="mb10">なし</div> : blog.tags.map(tag => {
+                      return <Chip tag={tag}></Chip>
                     })
                   }
-                    </div>
-                  </div>
                 </div>
-              )
-            })
-          }
-        </div>
-      </div>
-    </div>
+              </div>
+            </div>
+          )
+        })
+      }
+    </>
   )
-}
+};
+
+const cardFrame = css({
+  height: '150px',
+  backgroundColor: 'white',
+  margin: '15px',
+})
+
+const leftImage = css({
+  width: '150px',
+  padding: '0 5px'
+})
+
+const box = css({
+  padding: '5px 10px'
+})
+
+const description = css({
+  color: '#99a9b0'
+})
+
+const tagContainer = css({
+  justifyContent: 'end',
+  flexWrap: 'wrap-reverse'
+})
---
+++ pages/components/Chip.js
@@ -0,0 +1,33 @@
+/** @jsxImportSource @emotion/react */
+
+import { css }  from '@emotion/react';
+
+export default function Chip({ tag }) {
+
+  return (
+    <div className="flex-align-center justify-between" key={tag.id} onClick={(e) => {moveTagPage(e, tag)}} css={chip}>
+      <div className="flex-align-center">
+        <img css={chipImage} src={process.env.CATEGORY_URL + tag.category.name.toLowerCase() + '/' + tag.src} />
+        <div css={tagName}>{tag.name}</div>
+      </div>
+    </div>
+  )
+};
+
+const chip = css({
+  margin: '3px',
+  borderRadius: '20px',
+  minWidth: '80px',
+  backgroundColor: '#f4f4f7',
+  padding: '5px 15px'
+})
+
+const chipImage = css({
+  width: '12px'
+})
+
+const tagName = css({
+  padding: '0 7px',
+  fontSize: '8px',
+  fontWeight: 'bold'
+})
